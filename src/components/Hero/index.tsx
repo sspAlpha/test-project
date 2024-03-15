5ee38d15924cc9f5bc2b967755c552c221c6414a
Sarah Spriggs <ssp@alpha-solutions.us>
2024-03-15 17:04:48 -0500

    Fixes formatting. Updates eslint config.

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -2,18 +2,18 @@ import * as React from 'react';
 import { Text, Field } from '@sitecore-jss/sitecore-jss-react';
 
 type HeroProps = {
-  fields: {
-    heading: Field<string>;
-  }
-}
+    fields: {
+        heading: Field<string>;
+    };
+};
 
-const Hero =(props:HeroProps):JSX.Element => {
-  return (
-    <div>
-      <p>Hero Component</p>
-      <Text field={props.fields.heading} />
-    </div>
-  )
-}
+const Hero = (props: HeroProps): JSX.Element => {
+    return (
+        <div>
+            <p>Hero Component</p>
+            <Text field={props.fields.heading} />
+        </div>
+    );
+};
 
 export default Hero;
